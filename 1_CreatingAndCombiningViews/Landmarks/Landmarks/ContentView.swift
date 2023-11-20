41a11ec2a384ba0cc21411dc9faf49edbdd0a118
Yoojin <41438361+Yoojin99@users.noreply.github.com>
2023-11-20 23:30:35 +0900

    [SwiftUI_Essentials] 지역 디테일 view 생성

--- 1_CreatingAndCombiningViews/Landmarks/Landmarks/ContentView.swift
+++ 1_CreatingAndCombiningViews/Landmarks/Landmarks/ContentView.swift
@@ -10,18 +10,35 @@ import SwiftUI
 
 struct ContentView: View { // View 프로토콜 채택
     var body: some View {
-        VStack(alignment: .leading) { // default: center
-            Text("Turtle Rock")
-                .font(.title) // modifiers. each returns a new view
-            HStack {
-                Text("Joshua Tree National Park")
-                    .font(.subheadline)
-                Spacer() // use the full width of device. parent view 의 모든 공간을 쓰도록 spacer view를 담고 있는 view 를 확장함
-                Text("California")
-                    .font(.subheadline)
+        VStack {
+            MapView()
+                .frame(height: 300) // width 는 자동으로 설정됨
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) { // default: center
+                Text("Turtle Rock")
+                    .font(.title) // modifiers. each returns a new view
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Spacer() // use the full width of device. parent view 의 모든 공간을 쓰도록 spacer view를 담고 있는 view 를 확장함
+                    Text("California")
+                }
+                .font(.subheadline) // stack 내 모든 요소에 적용됨
+                .foregroundStyle(.secondary)
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
             }
+            .padding()
+            
+            Spacer()
         }
-        .padding()
     }
 }
 
