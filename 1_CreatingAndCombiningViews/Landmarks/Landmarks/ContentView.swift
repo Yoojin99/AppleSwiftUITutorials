6a72f2e86fe5cfaa0ea632d4c6ce51815d824941
Yoojin <41438361+Yoojin99@users.noreply.github.com>
2023-11-20 22:55:58 +0900

    [SwiftUI_Essentials] Stack 으로 view 묶기

--- 1_CreatingAndCombiningViews/Landmarks/Landmarks/ContentView.swift
+++ 1_CreatingAndCombiningViews/Landmarks/Landmarks/ContentView.swift
@@ -10,9 +10,18 @@ import SwiftUI
 
 struct ContentView: View { // View 프로토콜 채택
     var body: some View {
-        Text("Hello SwiftUI!")
-            .font(.title) // modifiers. each returns a new view
-            .foregroundStyle(.black)
+        VStack(alignment: .leading) { // default: center
+            Text("Turtle Rock")
+                .font(.title) // modifiers. each returns a new view
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer() // use the full width of device. parent view 의 모든 공간을 쓰도록 spacer view를 담고 있는 view 를 확장함
+                Text("California")
+                    .font(.subheadline)
+            }
+        }
+        .padding()
     }
 }
 
