d60352034be6db64bb881de89017bd54c689663c
Yoojin <41438361+Yoojin99@users.noreply.github.com>
2023-11-25 21:57:27 +0900

    [Drawing paths and shapes] 배지 데이터 생성

---
+++ 1_CreatingAndCombiningViews/Landmarks/Landmarks/Views/HexagonParameters.swift
@@ -0,0 +1,57 @@
+//
+//  HexagonParameters.swift
+//  Landmarks
+//
+//  Created by YJ on 11/25/23.
+//
+
+import Foundation
+import CoreGraphics
+
+struct HexagonParameters {
+    struct Segment { // one side of hexagon
+        let line: CGPoint
+        let curve: CGPoint
+        let control: CGPoint
+    }
+    
+    // 각 side 는 이전 side가 끝난 곳에서 시작
+    // line point 로 직선으로 움직임
+    // 두번째 point 로 bezier curve 로 이동
+    // 세번째 point 는 curve 의 모양을 조정
+    
+    static let adjustment: CGFloat = 0.005
+    
+    static let segments = [
+        Segment(
+            line:    CGPoint(x: 0.60, y: 0.05),
+            curve:   CGPoint(x: 0.40, y: 0.05),
+            control: CGPoint(x: 0.50, y: 0.00)
+        ),
+        Segment(
+            line:    CGPoint(x: 0.05, y: 0.20 + adjustment),
+            curve:   CGPoint(x: 0.00, y: 0.30 + adjustment),
+            control: CGPoint(x: 0.00, y: 0.25 + adjustment)
+        ),
+        Segment(
+            line:    CGPoint(x: 0.00, y: 0.70 - adjustment),
+            curve:   CGPoint(x: 0.05, y: 0.80 - adjustment),
+            control: CGPoint(x: 0.00, y: 0.75 - adjustment)
+        ),
+        Segment(
+            line:    CGPoint(x: 0.40, y: 0.95),
+            curve:   CGPoint(x: 0.60, y: 0.95),
+            control: CGPoint(x: 0.50, y: 1.00)
+        ),
+        Segment(
+            line:    CGPoint(x: 0.95, y: 0.80 - adjustment),
+            curve:   CGPoint(x: 1.00, y: 0.70 - adjustment),
+            control: CGPoint(x: 1.00, y: 0.75 - adjustment)
+        ),
+        Segment(
+            line:    CGPoint(x: 1.00, y: 0.30 + adjustment),
+            curve:   CGPoint(x: 0.95, y: 0.20 + adjustment),
+            control: CGPoint(x: 1.00, y: 0.25 + adjustment)
+        )
+    ]
+}
