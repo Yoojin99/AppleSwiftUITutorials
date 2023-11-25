15d940b8b4732fdd9a6050772864bc808c88141f
Yoojin <41438361+Yoojin99@users.noreply.github.com>
2023-11-25 16:14:00 +0900

    [List and Navigation] landmark list 생성

---
+++ 1_CreatingAndCombiningViews/Landmarks/Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,21 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by YJ on 11/25/23.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+#Preview {
+    LandmarkList()
+}
