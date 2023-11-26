19560a0d14fe8f4c213e3f41197ff8047e38ab1f
Yoojin <41438361+Yoojin99@users.noreply.github.com>
2023-11-26 18:07:56 +0900

    [Working with UI controls] edit 모드 추가

--- 1_CreatingAndCombiningViews/Landmarks/Landmarks/Views/Profiles/ProfileHost.swift
+++ 1_CreatingAndCombiningViews/Landmarks/Landmarks/Views/Profiles/ProfileHost.swift
@@ -8,15 +8,28 @@
 import SwiftUI
 
 struct ProfileHost: View {
+    @Environment(\.editMode) var editMode // @environment 로 접근할 수 있는 값들(envrionment 에 저장되어 있음)제공
+    @Environment(ModelData.self) var modelData
     @State private var draftProfile = Profile.default
     
     var body: some View {
         VStack(alignment: .leading, spacing: 20, content: {
-            ProfileSummary(profile: draftProfile)
+            HStack {
+                Spacer()
+                EditButton()
+            }
+            
+            if editMode?.wrappedValue == .inactive {
+                ProfileSummary(profile: modelData.profile)
+            } else {
+                Text("Profile Editor")
+            }
+            
         })
     }
 }
 
 #Preview {
     ProfileHost()
+        .environment(ModelData())
 }
