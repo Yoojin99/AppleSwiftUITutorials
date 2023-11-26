5a02e294bcd10c7743806610ea2ba383e5dd1b4a
Yoojin <41438361+Yoojin99@users.noreply.github.com>
2023-11-26 16:27:35 +0900

    [Composing complex interface] 카테고리 view 완성

--- 1_CreatingAndCombiningViews/Landmarks/Landmarks/Views/Categories/CategoryHome.swift
+++ 1_CreatingAndCombiningViews/Landmarks/Landmarks/Views/Categories/CategoryHome.swift
@@ -13,9 +13,17 @@ struct CategoryHome: View {
     var body: some View {
         NavigationSplitView {
             List {
+                modelData.features[0].image
+                    .resizable()
+                    .scaledToFill()
+                    .frame(height: 200)
+                    .clipped()
+                    .listRowInsets(EdgeInsets()) // edge inset 0으로 줘서 리스트 화면에 꽉 차게 만듦
+                
                 ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
-                    Text(key)
+                    CategoryRow(categoryName: key, items: modelData.categories[key]!)
                 }
+                .listRowInsets(EdgeInsets())
             }
             .navigationTitle("Featured")
         } detail: {
