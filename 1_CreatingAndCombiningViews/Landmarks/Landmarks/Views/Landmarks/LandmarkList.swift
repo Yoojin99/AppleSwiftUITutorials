792d8a5d52a3ac74b1a6e6ec6fde2d7c9034ea7a
Yoojin <41438361+Yoojin99@users.noreply.github.com>
2023-11-27 23:24:24 +0900

    [Creating a macOS app] custom menu command 추가

--- 1_CreatingAndCombiningViews/Landmarks/Landmarks/Views/Landmarks/LandmarkList.swift
+++ 1_CreatingAndCombiningViews/Landmarks/Landmarks/Views/Landmarks/LandmarkList.swift
@@ -11,6 +11,7 @@ struct LandmarkList: View {
     @Environment(ModelData.self) var modelData // environment(_:) 가 부모에 적용된 한 자동으로 값을 가져옴
     @State private var showFavoritesOnly = false
     @State private var filter = FilterCategory.all
+    @State private var selectedLandmark: Landmark?
     
     enum FilterCategory: String, CaseIterable, Identifiable {
             case all = "All"
@@ -34,9 +35,15 @@ struct LandmarkList: View {
         return showFavoritesOnly ? "Favorite \(title)" : title
     }
     
+    var index: Int? {
+        modelData.landmarks.firstIndex(where: { $0.id == selectedLandmark?.id })
+    }
+    
     var body: some View {
+        @Bindable var modelData = modelData
+        
         NavigationSplitView {
-            List {
+            List(selection: $selectedLandmark) {
                 // 1. static + dynamic view 결합
                 // 2. 둘 이상의 다른 dynamic view 그룹을 묶을 때
                 ForEach(filteredLandmarks) { landmark in
@@ -45,6 +52,7 @@ struct LandmarkList: View {
                     } label: {
                         LandmarkRow(landmark: landmark)
                     }
+                    .tag(landmark)
                 }
             }
             .navigationTitle(title)
@@ -70,6 +78,7 @@ struct LandmarkList: View {
         } detail: {
             Text("Select a Landmark") // 선택한 후에 나타나는 view의 placeholder. iPhone 에서는 필요없지만 iPad 에서는 선택전에 detail pane 이 보일 수도 있음
         }
+        .focusedValue(\.selectedLandmark, $modelData.landmarks[index ?? 0])
     }
 }
 
