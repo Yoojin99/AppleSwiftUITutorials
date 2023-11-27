e86c385058712104ab0c3e9e49e928bac5c1e9c6
Yoojin <41438361+Yoojin99@users.noreply.github.com>
2023-11-27 23:12:47 +0900

    [Creating a macOS app] built-in menu command 추가

---
+++ 1_CreatingAndCombiningViews/Landmarks/MacLandmarks/LandmarkCommands.swift
@@ -0,0 +1,14 @@
+//
+//  LandmarkCommands.swift
+//  MacLandmarks
+//
+//  Created by YJ on 11/27/23.
+//
+
+import SwiftUI
+
+struct LandmarkCommands: Commands {
+    var body: some Commands {
+        SidebarCommands() // command for toggling sidebar
+    }
+}
